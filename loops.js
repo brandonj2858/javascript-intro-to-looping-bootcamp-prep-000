05aa701650f03947c5629b282d9d4626d0af0ff9
Brandon James <brandonj2858@gmail.com>
2019-04-12 13:22:41 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,11 +19,11 @@ function whileLoop(n) {
    function incrementVariable() {
      i = i + 1;
      return i
-   }}
+   }
    
    
    do {console.log("I run once regardless.")}
-   while (incrementVariable() < num) 
+   while (incrementVariable() < num)}
    
  
   
