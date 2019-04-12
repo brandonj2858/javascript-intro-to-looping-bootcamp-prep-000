2b956cd6983ffaa5432172271888a03d1e8749cf
Brandon James <brandonj2858@gmail.com>
2019-04-12 12:50:39 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,8 +15,7 @@ function whileLoop(n) {
  return "done"} 
  
  function doWhileLoop(num) {
-   do {console.log("I run once regardless.")}
-  while (incrementVariable() < num)}
+   var i = 
  
   
 
